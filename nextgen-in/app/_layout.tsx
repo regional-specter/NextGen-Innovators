8b74f0114f692a6aeaa987f554ed65510a13dc27
Rao <103417697+Aby-ss@users.noreply.github.com>
2025-11-10 19:27:31 +0400

    Added splash screen

--- nextgen-in/app/_layout.tsx
+++ nextgen-in/app/_layout.tsx
@@ -2,12 +2,18 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import * as SplashScreen from 'expo-splash-screen';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
   anchor: '(tabs)',
 };
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -21,4 +27,6 @@ export default function RootLayout() {
       <StatusBar style="auto" />
     </ThemeProvider>
   );
+
+  return <Stack />;
 }
